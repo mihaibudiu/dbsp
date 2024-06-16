19f540d46732dd7b97275c460b289812ce9b6980
Mihai Budiu <mbudiu@feldera.com>
2024-06-15 17:35:02 -0700

    [SQL] Use join_flatmap when possible
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

---
+++ sql-to-dbsp-compiler/SQL-compiler/src/main/java/org/dbsp/sqlCompiler/circuit/operator/DBSPJoinFlatmapOperator.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2022 VMware, Inc.
+ * SPDX-License-Identifier: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.dbsp.sqlCompiler.circuit.operator;
+
+import org.dbsp.sqlCompiler.compiler.frontend.calciteObject.CalciteObject;
+import org.dbsp.sqlCompiler.compiler.visitors.VisitDecision;
+import org.dbsp.sqlCompiler.compiler.visitors.outer.CircuitVisitor;
+import org.dbsp.sqlCompiler.ir.expression.DBSPExpression;
+import org.dbsp.sqlCompiler.ir.type.DBSPType;
+import org.dbsp.sqlCompiler.ir.type.user.DBSPTypeZSet;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Objects;
+
+/** We use join_flatmap to implement a join followed by a filter.
+ * The function returns None for dropping a value, and Some(result)
+ * for keeping a result. */
+public final class DBSPJoinFlatmapOperator extends DBSPOperator {
+    public DBSPJoinFlatmapOperator(
+            CalciteObject node, DBSPTypeZSet outputType,
+            DBSPExpression function, boolean isMultiset,
+            DBSPOperator left, DBSPOperator right) {
+        super(node, "join_flatmap", function, outputType, isMultiset);
+        this.addInput(left);
+        this.addInput(right);
+    }
+
+    @Override
+    public DBSPOperator withFunction(@Nullable DBSPExpression expression, DBSPType outputType) {
+        return new DBSPJoinFlatmapOperator(
+                this.getNode(), outputType.to(DBSPTypeZSet.class),
+                Objects.requireNonNull(expression),
+                this.isMultiset, this.inputs.get(0), this.inputs.get(1));
+    }
+
+    @Override
+    public DBSPOperator withInputs(List<DBSPOperator> newInputs, boolean force) {
+        if (force || this.inputsDiffer(newInputs))
+            return new DBSPJoinFlatmapOperator(
+                    this.getNode(), this.getOutputZSetType(),
+                    this.getFunction(), this.isMultiset, newInputs.get(0), newInputs.get(1));
+        return this;
+    }
+
+    @Override
+    public void accept(CircuitVisitor visitor) {
+        visitor.push(this);
+        VisitDecision decision = visitor.preorder(this);
+        if (!decision.stop())
+            visitor.postorder(this);
+        visitor.pop(this);
+    }
+}
