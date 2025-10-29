feldera/feldera

Parse program dataflow graph
Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

profiler/src/index.ts
@@ -1,3 +1,3 @@
 import { Globals } from "./globals.js";
 
-Globals.getInstance().start("data/rec.json");
+Globals.getInstance().start("data/rec.json", "data/dataflow-rec.json");

mihaibudiu/dbsp

profiler/src/index.ts
@@ -1,3 +1,3 @@
 import { Globals } from "./globals.js";
 
-Globals.getInstance().start("data/rec.json");
+Globals.getInstance().start("data/join.json", "data/dataflow-join.json");